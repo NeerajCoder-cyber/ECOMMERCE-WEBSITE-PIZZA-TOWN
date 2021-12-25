62f1aa4acfc53ca22c6d6c1003adefdf36517b47
NeerajCoder-cyber <vashishtnr@gmail.com>
2021-12-26 01:22:03 +0530

    added procfile and ports

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ let port= process.env.PORT;
 if(port==null||port==""){
   port=3000;
 }
-
 app.listen(port, function() {
   console.log("Server started on port successfully");
 });
-
-//  res.render("veg");
-//  res.render("nonveg");
